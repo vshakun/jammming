cfbdd9b39b8108d1062bcdf1bb66f271d60dd0b0
Veronika Shakun <veronika.shakun@gmail.com>
2020-10-22 17:52:20 +0300

    Create static components

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Track.css';
+
+export class Track extends React.Component {
+    renderAction() {
+        if (this.isRemoval) {
+            return <button className="Track-action">-</button>;
+        } else {
+            return <button className="Track-action">+</button>;
+        }
+    }
+
+    render() {
+        return (
+        <div className="Track">
+            <div className="Track-information">
+                <h3>{/*<!-- track name will go here -->*/}</h3>
+                <p>{/*!-- track artist will go here--> | <!-- track album will go here -->*/}</p>
+            </div>
+            <button className="Track-action">{this.renderAction}</button>
+        </div>
+        );
+    }
+}
