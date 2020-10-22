a3b3865aedf6dc4fbc5f51e008ada49547f642c4
Veronika Shakun <veronika.shakun@gmail.com>
2020-10-22 20:24:50 +0300

    Pass down Playlist to TrackList

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './App.css';
 import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchResults } from '../SearchResults/SearchResults';
-//import { Playlist } from '../../Components/Playlist';
+import { Playlist } from '../Playlist/Playlist';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1},
+      {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
+
+      playlistName: 'New Playlist',
+
+      playlistTracks: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1},
       {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, {name: 'name3', artist: 'artist3', album: 'album3', id: 3}]
+
     };
   }
 
@@ -20,7 +27,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} />
-            {/*Add a Playlist component*/}
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
     </div>
   </div>
 </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Playlist.css';
-//import { TrackList } from '../Components/TrackList';
+import { TrackList } from '../TrackList/TrackList';
 
 export class Playlist extends React.Component {
     render() {
         return (
         <div className="Playlist">
             <input defaultValue={"New Playlist"}/>
-            {/*<!-- Add a TrackList component -->*/}
+            <TrackList tracks={this.props.playlistTracks} />
             <button className="Playlist-save">SAVE TO SPOTIFY</button>
         </div>
         );
