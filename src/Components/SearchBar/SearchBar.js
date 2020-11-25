8757c92c1b99b143fd0ea76ddfd3608ffda7cb9f
Veronika Shakun <veronika.shakun@gmail.com>
2020-11-25 16:05:25 +0300

    Search by Enter

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -21,10 +21,16 @@ export class SearchBar extends React.Component {
         this.setState({term: event.target.value});
     }
 
+    handleKeyPress(e) {
+        if (e.key === 'Enter') {
+            this.search(); 
+        }
+    }
+
     render() {
         return (
         <div className="SearchBar">
-            <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
+            <input onChange={this.handleTermChange} onKeyUp={this.handleKeyPress.bind(this)} placeholder="Enter A Song, Album, or Artist" />
             <button className="SearchButton" onClick={this.search}>SEARCH</button>
         </div>
         );
