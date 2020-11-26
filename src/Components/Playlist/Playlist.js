843ed1a06faa5568f0228dbfa3353ec244687217
Veronika Shakun <veronika.shakun@gmail.com>
2020-11-26 17:20:09 +0300

    Change name of the playlist after saving

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -7,21 +7,27 @@ export class Playlist extends React.Component {
         super(props);
 
         this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleSave = this.handleSave.bind(this);
     }
 
     handleNameChange(event) {
         this.props.onNameChange(event.target.value);
     }
 
+    handleSave() {
+        this.props.onSave();
+        document.getElementById('playlist-input').value = 'New Playlist';
+    }
+
     render() {
         return (
         <div className="Playlist">
-            <input defaultValue={"New Playlist"} onChange={this.handleNameChange} />
+            <input id='playlist-input' defaultValue={"New Playlist"} onChange={this.handleNameChange} />
             <TrackList tracks={this.props.playlistTracks} 
                     onRemove={this.props.onRemove} 
                     isRemoval={true} 
             />
-            <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
+            <button className="Playlist-save" onClick={this.handleSave}>SAVE TO SPOTIFY</button>
         </div>
         );
     }
